7544c1e6627101e696c1844011ceb4cc23ed5953
oroubagh <orouba24@gmail.com>
2023-05-08 20:53:07 +0300

    Edit Code

---
+++ src/components/About/About.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+// export  function About() {
+
+//  render(){
+//     return (
+//         <div>About</div>
+//       )
+//  }
+ 
+// }
+
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>About</div>
+  )
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,31 @@
+// import React from 'react'
+
+// export default function Home() {
+
+    
+
+//     render(){
+//         return (
+//             <div>Home</div>
+//           )
+//      }
+// }
+
+// import React from 'react'
+
+// function Home() {
+//   return (
+//     <div>Home</div>
+//   )
+// }
+
+// export default Home
+// No Need for render class here. 
+
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>Home</div>
+    )
+}
